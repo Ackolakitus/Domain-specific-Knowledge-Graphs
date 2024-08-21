f121f97f9fcf2a613c1fc6f06a051f5288911621
Ackolakitus <aleksandar.ristovski001@gmail.com>
2024-08-21 23:32:33 +0200

    Add changes

---
+++ drugs/Neo4jDrugsGraphClass.py
@@ -0,0 +1,307 @@
+import neo4j.exceptions
+from neo4j import GraphDatabase
+
+# CREATE INDEX FOR (p:Plant) ON (p.scientific_name);
+def create_constraints(tx):
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (k:Kingdom) REQUIRE k.name IS UNIQUE ")
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (spc:Superclass) REQUIRE spc.name IS UNIQUE ")
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (c:Class) REQUIRE c.name IS UNIQUE ")
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (sbc:Subclass) REQUIRE sbc.name IS UNIQUE ")
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (p:Parent) REQUIRE p.name IS UNIQUE ")
+    tx.run("CREATE CONSTRAINT IF NOT EXISTS FOR (d:Drug) REQUIRE d.name IS UNIQUE")
+
+
+def add_root_node(tx):
+    """
+    Add a root node labeled 'Root' with the name 'Kingdoms'.
+    """
+    query = "MERGE (r:Root {name: 'Kingdoms'})"
+    tx.run(query)
+
+
+def add_kingdom_nodes(tx, kingdoms):
+    """
+    Add kingdom node for each kingdom in the list.
+    """
+    for kingdom in kingdoms:
+        query = "MERGE (k:Kingdom {name: $name})"
+        try:
+            tx.run(query, name=kingdom)
+        except neo4j.exceptions.TransactionError:
+            continue
+
+def add_superclass_nodes(tx, superclasses):
+    """
+    Add superclass node for each superclass in the list.
+    """
+    for superclass in superclasses:
+        query = "MERGE (spc:Superclass {name: $name})"
+        try:
+            tx.run(query, name=superclass)
+        except neo4j.exceptions.TransactionError:
+            continue
+
+def add_class_nodes(tx, classes):
+    """
+    Add class node for each class in the list.
+    """
+    for cls in classes:
+        query = "MERGE (c:Class {name: $name})"
+        try:
+            tx.run(query, name=cls)
+        except neo4j.exceptions.TransactionError:
+            continue
+
+def add_subclass_nodes(tx, subclasses):
+    """
+    Add subclass node for each subclass in the list.
+    """
+    for subclass in subclasses:
+        query = "MERGE (sbc:Subclass {name: $name})"
+        try:
+            tx.run(query, name=subclass)
+        except neo4j.exceptions.TransactionError:
+            continue
+
+def add_or_update_drug_nodes(tx, drugs):
+    """
+    Add drug nodes with given attributes.
+    """
+    for drug in drugs:
+        query = (
+            "MERGE (d:Drug {name: $name}) "
+            "SET d.drugbank-id=$id d.type = $type, d.state = $state, d.groups = $groups, d.salts = $salts, d.affected_organisms = $affected_organisms, d.external_links = $external_links"
+        )
+        try:
+            tx.run(query, name=drug['name'], id=drug['drugbank-id'], type=drug['type'],
+                   state=drug['state'], groups=drug['groups'], salts=drug['salts'], affected_organisms=drug['affected_organisms'], external_links=drug['external_links'])
+        except neo4j.exceptions.TransactionError:
+            continue
+
+
+def add_or_update_relationships(tx, relationships):
+    """
+    Create relationship between any 2 types of nodes.
+    """
+    for rel in relationships:
+        query = (
+            "MATCH (a:$a_type {name: $a_name}), (b:$b_type {name: $b_name})"
+            "MERGE (a)-[:HAS_$relationship]->(b)"
+        )
+        tx.run(query, a_type=rel[0], a_name=rel[1], b_type=rel[2], b_name=rel[3], relationship=str(rel[2]).upper())
+
+
+def delete_drug_node(tx, drug_name):
+    """
+    Delete a drug node by its name.
+    """
+    query = """
+    MATCH (n:Drug {name: $name})
+    DETACH DELETE n
+    """
+    tx.run(query, name=drug_name)
+
+
+def delete_any_node(tx, node_type, node_name):
+    """
+    Delete any node type by its name.
+    """
+    query = """
+    MATCH (n:$type {name: $name})
+    DETACH DELETE n
+    """
+    tx.run(query, type=node_type, name=node_name)
+
+
+def delete_relationship(tx, relationship):
+    """
+    Delete the relationship between 2 nodes of any type.
+    """
+    if len(relationship) != 4:
+        print("Invalid relationship format")
+        return
+
+    query = """
+            MATCH (a:$a_type {name: $a_name})-[r:HAS_$relationship]->(b:$b_type {name: $b_name})
+            DELETE r
+            """
+    tx.run(query,
+           a_type=relationship[0],
+           a_name=relationship[1],
+           b_type=relationship[2],
+           b_name=relationship[3],
+           relationship=str(relationship[2]).upper())
+
+# MATCH (s:Superclass)-[r*0..]->(n)
+# WHERE s.name = $superclassName
+# AND (type(r) IN ['HAS_CLASS', 'HAS_SUBCLASS', 'HAS_DRUG'])
+# RETURN DISTINCT n
+
+def delete_node_and_nodes_belonging(tx, node_type, node_name):
+    """
+    Delete a type of node by its name and all nodes belonging to it.
+    """
+    delete_nodes_query = """
+            MATCH (p:Plant)-[:BELONGS_TO]->(n:$type {name: $name})
+            DETACH DELETE p
+            """
+    tx.run(delete_nodes_query, type=node_type, name=node_name)
+
+    delete_node_query = """
+            MATCH (n:$type {name: $name})
+            DETACH DELETE n
+            """
+    tx.run(delete_node_query, type=node_type, name=node_name)
+
+
+def print_plant_node_details(node):
+    (node_id, node_props) = node
+    print(f"Plant: {node_props['scientific_name']}\n"
+          f"Symbol: {node_props['symbol']}\n"
+          f"Common name: {node_props['common_name']}\n"
+          f"Other names: {node_props['other_names']}\n"
+          f"Authors: {node_props['authors']}")
+
+
+class Neo4jGraphClass:
+    def __init__(self, uri, user, password):
+        """
+        Initialize the Neo4jGraphClass with the provided URI, user, and password.
+        """
+        self.uri = uri
+        self.user = user
+        self.password = password
+        self.driver = None
+
+    def __enter__(self):
+        """
+        Establish a connection to the Neo4j database.
+        """
+        try:
+            self.driver = GraphDatabase.driver(self.uri, auth=(self.user, self.password))
+            self.driver.verify_connectivity()
+        except neo4j.exceptions.ConfigurationError:
+            print("URI format is not supported! Check your uri environment variable.")
+        except neo4j.exceptions.AuthError:
+            print("Authentication failure! Check your auth environment variables.")
+        except neo4j.exceptions.ServiceUnavailable:
+            print("Could not establish a connection with Neo4j database!")
+        except ValueError:
+            print("Unknown exception")
+
+        with self.driver.session() as session:
+            # try:
+            session.write_transaction(create_constraints)
+            # except neo4j.exceptions.ClientError as e:
+            #     print(f"{e.message}")
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Close the connection to the Neo4j database.
+        """
+        if self.driver:
+            self.driver.close()
+
+    def createOrUpdateGraph(self, drugs, kingdoms, superclasses, classes, subclasses, relationships, batch_size=300):
+        """
+        Save the graph data into the Neo4j database.
+
+        Steps:\n
+        1. Add a root node labeled 'Root' with the name 'Kingdoms'.
+        2. Add classification nodes to the root node in batches.
+        3. Add drug nodes to the classification nodes in batches.
+        4. Add relationships between nodes in batches.
+
+        :param drugs: List of drugs dictionaries with attributes.
+        :param kingdoms: List of kingdoms names.
+        :param superclasses: List of superclasses names.
+        :param classes: List of classes names.
+        :param subclasses: List of subclasses names.
+        :param relationships: List of all relationships .
+        :param batch_size: Number of items to process per batch (default: 300).
+        """
+        with self.driver.session() as session:
+            try:
+                session.execute_write(add_root_node)
+            except neo4j.exceptions.ConstraintError:
+                pass
+
+            try:
+                session.execute_write(add_family_nodes, families)
+            except neo4j.exceptions.ConstraintError:
+                pass
+
+            try:
+                session.execute_write(add_root_relationships, families)
+            except neo4j.exceptions.ConstraintError:
+                pass
+
+            def process_batches(items, batch_func):
+                total_items = len(items)
+                for i in range(0, total_items, batch_size):
+                    end_index = min(i + batch_size, total_items)
+                    batch = items[i:end_index]
+                    try:
+                        session.execute_write(batch_func, batch)
+                    except neo4j.exceptions.ConstraintError:
+                        pass
+                    print(f"Processed items from {i} to {i + len(batch)}")
+
+            process_batches(plants, add_or_update_plant_nodes)
+            process_batches(relationships, add_or_update_relationships)
+
+    def deleteDataFromGraph(self, plants, families, deleteFamily):
+        with self.driver.session() as session:
+            if deleteFamily:
+                session.execute_write(delete_family_and_nodes_belonging, families)
+            else:
+                for plant in plants:
+                    session.execute_write(delete_plant_node, plant)
+
+    def get_plants_nodes_belonging_to_family(self, family_name):
+        """
+        Get plant nodes belonging to a specific family.
+        :param family_name: Name of the family to search for.
+        :return: List of nodes belonging to the family.
+        """
+        query = """
+                MATCH (n)-[:BELONGS_TO]->(m {name: $name})
+                RETURN elementId(n) AS node_id, n
+                """
+        with self.driver.session() as session:
+            result = session.run(query, name=family_name)
+            nodes = [(record["node_id"], record["n"]) for record in result]
+        return nodes
+
+    def get_plant_node(self, plantName):
+        query = """
+            MATCH (p:Plant {scientific_name: $scientific_name})
+            RETURN elementId(p) AS node_id, p
+        """
+        with self.driver.session() as session:
+            result = session.run(query, scientific_name=plantName)
+            record = result.single()
+            node = (record["node_id"], record["p"])
+        return node
+
+    def deleteFamilyNode(self, familyName):
+        with self.driver.session() as session:
+            session.execute_write(delete_family_node, familyName)
+
+    def deletePlantNode(self, plantName):
+        with self.driver.session() as session:
+            session.execute_write(delete_plant_node, plantName)
+
+    def deleteFamilyAndNodesBelonging(self, familyName):
+        with self.driver.session() as session:
+            session.execute_write(delete_family_and_nodes_belonging, familyName)
+
+# with Neo4jGraphClass(uri, user, password) as neo4j_query:
+# nodes = neo4j_query.find_nodes_belonging_to_family("Moringaceae")
+# nodes = neo4j_query.find_nodes_belonging_to_family("Apiaceae")
+# for node in nodes:
+#     print(node)
+# print(node["n"]["element_id"])
+# print(node['node_id'])
--- drugs/extract_data.py
+++ drugs/extract_data.py
@@ -1,29 +1,33 @@
+import sys
 import xml.etree.ElementTree as ET
 import pickle
 import json
+from collections import defaultdict
 
 
 def save_to_pickle(data, file_path):
     with open(file_path, 'wb') as pickle_file:
         pickle.dump(data, pickle_file)
 
+
 def load_from_json(file_path):
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
     return data
 
-def load_from_pickle(file_path):
-    with open(file_path, 'rb') as pickle_file:
-        data = pickle.load(pickle_file)
-    return data
 
 def save_to_json(data, file_path):
     with open(file_path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
 
+def load_from_pickle(file_path):
+    with open(file_path, 'rb') as pickle_file:
+        data = pickle.load(pickle_file)
+    return data
+
+
 def extract_drug_info(file_path):
-    # Define the namespace
     ns = {'drugbank': 'http://www.drugbank.ca'}
 
     tree = ET.parse(file_path)
@@ -32,16 +36,20 @@ def extract_drug_info(file_path):
     kingdoms = set()
     superclasses = set()
     classes = set()
-    drugs_info = []
+    subclasses = set()
+    parents = set()
+
+    biotech_info = []
+    small_molecules_info = []
 
     for drug in root.findall('drugbank:drug', ns):
-
+        type = drug.attrib.get('type')
         classification = drug.find('drugbank:classification', ns)
 
         if classification is not None:
             # Add to respective sets
             if classification.find('drugbank:kingdom', ns) is not None:
-                kingdoms.add(classification.find('drugbank:kingdom', ns).text)
+                kingdoms.add(classification.find('drugbank:kingdom', ns).text.title())
 
             if classification.find('drugbank:superclass', ns) is not None:
                 superclasses.add(classification.find('drugbank:superclass', ns).text)
@@ -49,95 +57,181 @@ def extract_drug_info(file_path):
             if classification.find('drugbank:class', ns) is not None:
                 classes.add(classification.find('drugbank:class', ns).text)
 
+            if classification.find('drugbank:subclass', ns) is not None:
+                subclasses.add(classification.find('drugbank:subclass', ns).text)
+
+            if classification.find('drugbank:direct-parent', ns) is not None:
+                parents.add(classification.find('drugbank:direct-parent', ns).text)
+
         drug_info = {
+            'drugbank-id': drug.find('drugbank:drugbank-id', ns).text,
             'type': drug.attrib.get('type'),
             'name': drug.find('drugbank:name', ns).text if drug.find('drugbank:name', ns) is not None else None,
             'state': drug.find('drugbank:state', ns).text if drug.find('drugbank:state', ns) is not None else None,
+            'groups': [group.text for group in drug.findall('drugbank:groups/drugbank:group', ns)],
+            'salts': [{'drugbank-id': salt.find('drugbank:drugbank-id', ns).text,
+                       'name': salt.find('drugbank:name', ns).text}
+                      for salt in drug.findall('drugbank:salts/drugbank:salt', ns)],
             'classification': {
-                'direct-parent': classification.find('drugbank:direct-parent',
-                                                     ns).text if classification is not None else None,
-                'kingdom': classification.find('drugbank:kingdom', ns).text if classification is not None else None,
+                'kingdom': classification.find('drugbank:kingdom',
+                                               ns).text.title() if classification is not None else None,
                 'superclass': classification.find('drugbank:superclass',
                                                   ns).text if classification is not None else None,
                 'class': classification.find('drugbank:class', ns).text if classification is not None else None,
                 'subclass': classification.find('drugbank:subclass', ns).text if classification is not None else None,
+                'parent': classification.find('drugbank:direct-parent',
+                                              ns).text if classification is not None else None,
             } if classification is not None else None,
-            'synonyms': [syn.text for syn in drug.findall('drugbank:synonyms/drugbank:synonym', ns)],
             'affected_organisms': [organism.text for organism in
                                    drug.findall('drugbank:affected-organisms/drugbank:affected-organism', ns)],
-            'dosages': [{'form': dosage.find('drugbank:form', ns).text,
-                         'route': dosage.find('drugbank:route', ns).text,
-                         'strength': dosage.find('drugbank:strength', ns).text}
-                        for dosage in drug.findall('drugbank:dosages/drugbank:dosage', ns)],
             'food_interactions': [interaction.text for interaction in
                                   drug.findall('drugbank:food-interactions/drugbank:food-interaction', ns)],
             'drug_interactions': [{'name': interaction.find('drugbank:name', ns).text,
                                    'description': interaction.find('drugbank:description', ns).text}
                                   for interaction in
                                   drug.findall('drugbank:drug-interactions/drugbank:drug-interaction', ns)],
-            'external_link': drug.find('drugbank:external-link', ns).text if drug.find('drugbank:external-link',
-                                                                                       ns) is not None else None,
-            'pathways': [{'name': pathway.find('drugbank:name', ns).text,
-                          'smpdb-id': pathway.find('drugbank:smpdb-id', ns).text,
-                          'category': pathway.find('drugbank:category', ns).text}
-                         for pathway in drug.findall('drugbank:pathways/drugbank:pathway', ns)],
+            'external_links': drug.find('drugbank:external-link', ns).text if drug.find('drugbank:external-link',
+                                                                                        ns) is not None else None,
+            # 'pathways': [{'name': pathway.find('drugbank:name', ns).text,
+            #               'smpdb-id': pathway.find('drugbank:smpdb-id', ns).text,
+            #               'category': pathway.find('drugbank:category', ns).text}
+            #              for pathway in drug.findall('drugbank:pathways/drugbank:pathway', ns)],
         }
 
-        drugs_info.append(drug_info)
+        if type == 'biotech':
+            biotech_info.append(drug_info)
+        elif type == 'small molecule':
+            small_molecules_info.append(drug_info)
 
-    return drugs_info, kingdoms, superclasses, classes
+    return biotech_info, small_molecules_info, kingdoms, superclasses, classes, subclasses, parents
 
 
 # Example usage
-def createData():
+def create_data():
     file_path = './data/full database.xml'
-    drugs, kingdoms, superclasses, classes = extract_drug_info(file_path)
+    biotech_drugs, small_molecule_drugs, kingdoms, superclasses, classes, subclasses, parents = extract_drug_info(
+        file_path)
 
-    save_to_pickle(drugs, './data/extracted-drugs.pkl')
-    save_to_json(drugs, './data/extracted-drugs.json')
+    save_to_pickle(biotech_drugs, './data/extracted-biotech-drugs.pkl')
+    save_to_json(biotech_drugs, './data/extracted-biotech-drugs.json')
 
-    for kingdom in kingdoms:
-        print(kingdom)
-
-    for sc in superclasses:
-        print(sc)
-
-    for c in classes:
-        print(c)
+    save_to_pickle(small_molecule_drugs, './data/small-molecule-drugs.pkl')
+    save_to_json(small_molecule_drugs, './data/small-molecule-drugs.json')
 
 
 def extract_classification_sets(drugs):
-    kingdoms = set()
-    superclasses = set()
-    classes = set()
+    kingdoms = defaultdict(int)
+    superclasses = defaultdict(int)
+    classes = defaultdict(int)
+    subclasses = defaultdict(int)
 
     for drug in drugs:
         classification = drug.get('classification', {})
         if classification:
-            if classification.get('kingdom'):
-                kingdoms.add(classification['kingdom'])
-            if classification.get('superclass'):
-                superclasses.add(classification['superclass'])
-            if classification.get('class'):
-                classes.add(classification['class'])
+            if kingdom := classification.get('kingdom'):
+                kingdoms[kingdom] += 1
+            if superclass := classification.get('superclass'):
+                superclasses[superclass] += 1
+            if class_ := classification.get('class'):
+                classes[class_] += 1
+            if subclass := classification.get('subclass'):
+                subclasses[subclass] += 1
 
-    return kingdoms, superclasses, classes
+    return kingdoms, superclasses, classes, subclasses
 
-def printDataItems(data):
+
+def print_data_items(data):
     for item in data:
         print(item)
 
+
+def print_data_items_count(data):
+    for item, count in data.items():
+        print(f"{item}: {count} items")
+
+
+def extract_classification_relationships(drugs):
+    relationships_set = set()
+
+    for drug in drugs:
+        if drug.get('name', "") == "Ceftibuten":
+            print("EVEGO BE")
+
+        classification = drug.get('classification', {})
+        if classification:
+            if kingdom := classification.get('kingdom'):
+                relationships_set.add(('Root', 'Kingdoms', 'Kingdom', kingdom))
+            if superclass := classification.get('superclass'):
+                relationships_set.add(('Kingdom', kingdom, 'Superclass', superclass))
+            if class_ := classification.get('class'):
+                relationships_set.add(('Superclass', superclass, 'Class', class_))
+            if subclass := classification.get('subclass'):
+                relationships_set.add(('Class', class_, 'Subclass', subclass))
+            if direct_parent := classification.get('direct-parent'):
+                for key, value in classification.items():
+                    if key != "direct-parent" and value == direct_parent:
+                        parent_type = key.title()
+                        relationships_set.add((f'{parent_type}', direct_parent, 'Drug', drug.get('name')))
+                        break
+
+    return relationships_set
+
+def print_relations_neo4j(data):
+    for item in data:
+        print(f'MATCH (a:{item[0]} {{name: {item[1]}}}), (b:{item[2]} {{name: {item[3]}}})')
+        print(f'MERGE (a)-[:HAS_{str(item[2]).upper()}]->(b)')
+
+def extract_interaction_relationships(drugs, type):
+    relationships = []
+
+    if type not in ['drug', 'food']:
+        print("Invalid interactions type.")
+        sys.exit(1)
+
+    for drug in drugs:
+        interactions = drug.get(f'{type}_interactions', {})
+
+        drug_name = drug.get('name', "")
+
+        for i in interactions:
+            if type == 'drug':
+                relationships.append((drug_name, i.get('name', ""), i.get('description', "")))
+            elif type == 'food':
+                relationships.append((drug_name, i))
+
+    return relationships
+
 def main():
-    data = load_from_pickle('./data/extracted-drugs.pkl')
+    # create_data()
 
-    kingdoms, superclasses, classes = extract_classification_sets(data)
+    # data = load_from_json('./data/extracted-biotech-drugs.json')
+    data = load_from_json('./data/small-molecule-drugs.json')
 
-    print("=== KINGDOMS ===")
-    printDataItems(kingdoms)
-    print("=== SUPERCLASSES ===")
-    printDataItems(superclasses)
-    print("=== CLASSES ===")
-    printDataItems(classes)
+    # kingdoms, superclasses, classes, subclasses = extract_classification_sets(data)
+    #
+    # print("Kingdoms:")
+    # print_data_items_count(kingdoms)
+    #
+    # print("\nSuperclasses:")
+    # print_data_items_count(superclasses)
+    #
+    # print("\nClasses:")
+    # print_data_items_count(classes)
+    #
+    # print("\nSubclasses:")
+    # print_data_items_count(subclasses)
 
+    # relations = extract_classification_relationships(data)
+    # print_data_items(relations)
+    # print_relations_neo4j(relations)
+
+    # TOO MUCH
+    # drug_interactions = extract_interaction_relationships(data, 'drug')
+    # print_data_items(drug_interactions)
+    # print(len(drug_interactions))
+
+    food_interactions = extract_interaction_relationships(data, 'food')
+    print_data_items(food_interactions)
+    print(len(food_interactions))
 if __name__ == '__main__':
     main()
